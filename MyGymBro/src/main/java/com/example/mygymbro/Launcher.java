bed257f104b3fefaec5759187ba6432934af91db
david <davidetamburrini@gmail.com>
2026-01-17 12:23:30 +0100

    push copia di sicurezza funzionante

--- MyGymBro/src/main/java/com/example/mygymbro/Launcher.java
+++ MyGymBro/src/main/java/com/example/mygymbro/Launcher.java
@@ -4,5 +4,6 @@ public class Launcher {
     public static void main(String[] args) {
         // Invece di Application.launch, chiamiamo direttamente il main della nostra classe Main
         Main.main(args);
+        System.out.println("Launcher started the application.");
     }
 }
